da2e85caf5924a025b5d875500a6ced16bddc2ba
max <max@gmail.com>
2025-03-27 11:31:39 +0100

    Revert "dsl generated by junie"
    This reverts commit 46a2bf0934f11d39ce5eb02c1703cbe0a9aaf73d.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -28,70 +25,4 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2025.03"
 
 project {
-    // Use DslContext.settingsRoot as a VCS root
-    val vcsRoot = DslContext.settingsRoot
-
-    // Build configuration for the desktop application
-    buildType {
-        id("BuildDesktopApp")
-        name = "Build Desktop Application"
-
-        vcs {
-            root(vcsRoot)
-        }
-
-        steps {
-            gradle {
-                name = "Build Desktop Application"
-                tasks = "composeApp:packageDistributionForCurrentOS"
-                useGradleWrapper = true
-            }
-        }
-
-        triggers {
-            vcs {
-                branchFilter = "+:*"
-            }
-        }
-
-        features {
-            perfmon {}
-        }
-
-        artifactRules = """
-            composeApp/build/compose/binaries/main/*/org.example.project-*.* => desktop-app
-        """.trimIndent()
-    }
-
-    // Build configuration for the web application
-    buildType {
-        id("BuildWebApp")
-        name = "Build Web Application"
-
-        vcs {
-            root(vcsRoot)
-        }
-
-        steps {
-            gradle {
-                name = "Build Web Application"
-                tasks = "composeApp:wasmJsBrowserProductionWebpack"
-                useGradleWrapper = true
-            }
-        }
-
-        triggers {
-            vcs {
-                branchFilter = "+:*"
-            }
-        }
-
-        features {
-            perfmon {}
-        }
-
-        artifactRules = """
-            composeApp/build/dist/wasmJs/productionExecutable => web-app
-        """.trimIndent()
-    }
 }
